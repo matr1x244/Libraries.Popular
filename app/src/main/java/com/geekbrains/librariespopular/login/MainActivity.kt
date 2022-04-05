caaad2897a7495e9e2c4c5c3331fe248eebd59bf
matr1x244 <zio-21@yandex.ru>
2022-04-05 19:03:30 +0300

    вернул mpv с фрагмента на активити

--- app/src/main/java/com/geekbrains/librariespopular/login/MainActivity.kt
+++ app/src/main/java/com/geekbrains/librariespopular/login/MainActivity.kt
@@ -1,32 +1,38 @@
 package com.geekbrains.librariespopular.login
 
 import android.app.Activity
-import android.content.Context.INPUT_METHOD_SERVICE
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import com.geekbrains.librariespopular.R
-import com.geekbrains.librariespopular.databinding.FragmentLoginBinding
-import com.geekbrains.librariespopular.viewBindingFragment.ViewBindingFragment
+import com.geekbrains.librariespopular.databinding.ActivityMainBinding
 
-class LoginFragment: ViewBindingFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate),LoginContract.View {
-
-    companion object{
-        fun newInstance() = LoginFragment()
-    }
+class MainActivity : AppCompatActivity(), LoginContract.View {
 
     private var presenter: LoginContract.Presenter? = null
+    private lateinit var binding: ActivityMainBinding
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setRetainInstance(true) // сохраняем состоянии фрагмента
-        presenter = LoginPresenter()
-        presenter?.onAttach(this)
-
+        restorePresenter()
         loginButton()
+        presenter = restorePresenter()
+        presenter?.onAttach(this)
+    }
+
+    private fun restorePresenter(): LoginPresenter {
+        val presenter = lastCustomNonConfigurationInstance as? LoginPresenter
+        return presenter ?: LoginPresenter()
+    }
+
+    override fun onRetainCustomNonConfigurationInstance(): Any? {
+        return presenter
     }
 
     private fun loginButton() {
@@ -42,13 +48,12 @@ class LoginFragment: ViewBindingFragment<FragmentLoginBinding>(FragmentLoginBind
         binding.loginButton.isVisible = false
         binding.loginEditText.isVisible = false
         binding.passwordEditText.isVisible = false
-        binding.fragmentContainerLogin.setBackgroundColor(resources.getColor(R.color.color_gray_transparent))
-        Toast.makeText(context, "Успешно", Toast.LENGTH_SHORT).show()
-
+        binding.containerMainActivity.setBackgroundColor(Color.YELLOW)
+        Toast.makeText(this, "Успешно", Toast.LENGTH_SHORT).show()
     }
 
     override fun setError(error: String) {
-        Toast.makeText(context, "$error", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "$error", Toast.LENGTH_SHORT).show()
     }
 
     override fun showProgress() {
@@ -57,11 +62,12 @@ class LoginFragment: ViewBindingFragment<FragmentLoginBinding>(FragmentLoginBind
 
     override fun hideProgress() {
         binding.loginButton.isEnabled = true
-        hideKeyboard(requireActivity())
+        hideKeyboard(this)
     }
 
     private fun hideKeyboard(activity: Activity) {
-        val imm: InputMethodManager = activity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        val imm: InputMethodManager =
+            activity.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         var view: View? = activity.currentFocus
         if (view == null) {
             view = View(activity)
